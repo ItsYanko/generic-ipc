c6b29496e8daa4c7b1da94fe8f260f962cc2ee5d
ItsYanko <ianperina@gmail.com>
2021-03-05 22:09:46 +0000

    publish test

---
+++ classes/generic-ipc.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @class
+ * @name Server
+ * @description Initialize a new server
+ * @example
+ * const gIPC = require("generic-ipc");
+ * let instance = new gIPC(3000, {authorization: true})
+ */
+class Server{
+
+}
